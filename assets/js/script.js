738a625b33ba8e8cbe1b2534f173d3e16fb5945b
Will Darkins <willdarkins@gmail.com>
2021-09-13 08:34:04 -0700

    Basic API search structure impletmented

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
-var getCityForecasts = function() {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=London&appid=08c050bc124b048247b7377940b748b0");
-  };
-  
-  getCityForecasts();
+var getCityForecasts = function(city) {
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=08c050bc124b048247b7377940b748b0'
+
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        })
+    })
+}
+
+getCityForecasts('shanghai');
