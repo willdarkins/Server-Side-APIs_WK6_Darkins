f8f968c6e0768d9719c5e6b91a2e270d355a5aa7
Will Darkins <willdarkins@gmail.com>
2021-09-13 16:20:52 -0700

    small changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,37 @@ var userFormEl = document.querySelector('#user-form');
 var cityInputEl = document.querySelector('#cityname');
 var forecastContainerEl = document.querySelector('#forecast-container');
 var citySearchTermEl = document.querySelector('#city-search-term');
+var pastSearchEl = document.querySelector('#past-search')
+var fiveDayEl = document.querySelector('#five-day-forecast')
 
 
 var getCityForecasts = function (city) {
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=08c050bc124b048247b7377940b748b0'
+    var currentApiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=08c050bc124b048247b7377940b748b0'
 
-    fetch(apiUrl).then(function (response) {
+    fetch(currentApiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
                 displayWeather(data, city);
-                console.log(data);
             });
         } else {
             alert('ERROR: ' + city + ' does not have a forecast. Please input name of real city.')
         }
+    })
+}
+
+var getFiveDayForecast = function(city) {
+    var fiveDayApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&08c050bc124b048247b7377940b748b0='
+
+    fetch(fiveDayApiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                displayFiveDay(data, city);
+                console.log(data);
+            })
+        } else {
+            alert('ERROR: ' + city + ' does not have five-day forecast. Please input name of real city.')
+        }
+
     })
 }
 
@@ -35,7 +52,7 @@ var citySubmitHandler = function (event) {
 var displayWeather = function (weather, searchTerm) {
     forecastContainerEl.textContent = ''
     citySearchTermEl.textContent = searchTerm + ' (' + moment().format('l') + ')';
-
+    
     var temp = weather.main.temp;
     var tempEl = document.createElement('div');
     var tempContainer = document.createElement("span");
@@ -57,17 +74,10 @@ var displayWeather = function (weather, searchTerm) {
     humidityEl.appendChild(humidityContainer);
     forecastContainerEl.appendChild(humidityEl);
 
-    // var uvIndexEl = document.createElement('div');
-
-
-
 };
 
+var displayFiveDay = function(weather, searchTerm) {
+
+}
+
 userFormEl.addEventListener('submit', citySubmitHandler);
-
-
-
-{/* <div id="temp"> Temp: </div>
-<div id="wind"> Wind: </div>
-<div id="humidity"> Humidity: </div>
-<div id="uvIndex"> UV Index:</div> */}
